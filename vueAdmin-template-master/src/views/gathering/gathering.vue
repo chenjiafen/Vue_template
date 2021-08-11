3d658887d820a74d25f79cdad37c2aff13897017
chenjiafeng2 <M_chenjiafeng@163.com>
2021-08-11 20:03:35 +0800

    vue 标准模板

---
+++ vueAdmin-template-master/src/views/gathering/gathering.vue
@@ -0,0 +1,40 @@
+<template>
+  <el-table :data="list" border style="width: 100%">
+    <el-table-column prop="id" label="活动ID" width="180"></el-table-column>
+    <el-table-column prop="name" label="活动名称" width="180"></el-table-column>
+    <el-table-column prop="sponsor" label="主办方" width="180"></el-table-column>
+    <el-table-column prop="address" label="活动地址" width="180"></el-table-column>
+    <el-table-column prop="starttime" label="开始日期" width="180"></el-table-column>
+    <el-table-column prop="endtime" label="结束日期" width="180"></el-table-column>
+  </el-table>
+</template>
+
+<script>
+  import gatheringApi from '@/api/gathering'
+
+  export default {
+    data() {
+      return {
+        list: []
+      }
+    },
+    created() {
+      this.fetchData()
+    },
+    methods: {
+      fetchData() {
+        gatheringApi.getList().then(response => {
+          this.list = response.data
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+  .demo {
+    font-size: 28px;
+  }
+
+</style>
